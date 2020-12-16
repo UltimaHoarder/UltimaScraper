72a1802f760891831238f79fefcc41223621f983
CRIMINAL <datahoarders@protonmail.com>
2020-12-16 17:09:24 +0000

    Profiles Fix

--- start_ofd.py
+++ start_ofd.py
@@ -3,6 +3,7 @@ import tests.main_test as main_test
 
 main_test.version_check()
 main_test.check_config()
+main_test.check_profiles()
 
 if __name__ == "__main__":
     import datascraper.main_datascraper as main_datascraper
--- tests/main_test.py
+++ tests/main_test.py
@@ -1,3 +1,6 @@
+from apis.onlyfans.onlyfans import auth_details
+from genericpath import exists
+from re import L
 import sys
 import os
 
@@ -22,6 +25,37 @@ def check_config():
     if json_config != json_config2:
         input(
             f"The .settings\\{file_name} file has been updated. Fill in whatever you need to fill in and then press enter when done.\n")
+
+
+def check_profiles():
+    file_name = "config.json"
+    path = os.path.join('.settings', file_name)
+    import helpers.main_helper as main_helper
+    json_config, json_config2 = main_helper.get_config(path)
+    json_settings = json_config["settings"]
+    profile_directories = json_settings["profile_directories"]
+    profile_directory = profile_directories[0]
+    matches = ["OnlyFans"]
+    for match in matches:
+        q = os.path.abspath(profile_directory)
+        profile_site_directory = os.path.join(q, match)
+        if os.path.exists(profile_site_directory):
+            e = os.listdir(profile_site_directory)
+            e = [os.path.join(profile_site_directory, x, "auth.json")
+                 for x in e]
+            e = [x for x in e if os.path.exists(x)]
+            if e:
+                continue
+        default_profile_directory = os.path.join(
+            profile_site_directory, "default")
+        os.makedirs(default_profile_directory, exist_ok=True)
+        auth_filepath = os.path.join(default_profile_directory, "auth.json")
+        if not os.path.exists(auth_filepath):
+            x = auth_details().__dict__
+            main_helper.export_json(auth_filepath, x)
+            print
+        print
+    print
 
 
 # def check_extra_auth():

b4ae7e982a96842de4c2acac556f5fc37318311b
CRIMINAL <datahoarders@protonmail.com>
2021-05-05 18:38:54 +0100

    Signed Headers v7

--- apis/onlyfans/onlyfans.py
+++ apis/onlyfans/onlyfans.py
@@ -36,19 +36,19 @@ def create_signed_headers(link: str,  auth_id: int):
     path = urlparse(link).path
     query = urlparse(link).query
     path = path if not query else f"{path}?{query}"
-    static_param = "rhtNVxJh2LD3Jul5MhHcAAnFMysnLlct"
+    static_param = "cyo3uhuZp5tqYuEAhaVMuXMmPpRRsBq1"
     a = [static_param, time2, path, str(auth_id)]
     msg = "\n".join(a)
     message = msg.encode("utf-8")
     hash_object = hashlib.sha1(message)
     sha_1_sign = hash_object.hexdigest()
     sha_1_b = sha_1_sign.encode("ascii")
-    checksum = sum([sha_1_b[31], sha_1_b[13], sha_1_b[8], sha_1_b[3], sha_1_b[25], sha_1_b[8], sha_1_b[33], sha_1_b[25], sha_1_b[1], sha_1_b[23], sha_1_b[37], sha_1_b[11], sha_1_b[2], sha_1_b[29], sha_1_b[9], sha_1_b[7],
-					sha_1_b[29], sha_1_b[30], sha_1_b[18], sha_1_b[25], sha_1_b[18], sha_1_b[21], sha_1_b[10], sha_1_b[37],
-					sha_1_b[28], sha_1_b[35], sha_1_b[31], sha_1_b[5],
-					sha_1_b[13], sha_1_b[31], sha_1_b[2], sha_1_b[9]]) + 1110
+    checksum = sum([sha_1_b[25], sha_1_b[35], sha_1_b[26], sha_1_b[34], sha_1_b[32], sha_1_b[35], sha_1_b[32], sha_1_b[6], sha_1_b[21], sha_1_b[2], sha_1_b[15], sha_1_b[9], sha_1_b[3], sha_1_b[14], sha_1_b[26], sha_1_b[26],
+                    sha_1_b[5], sha_1_b[34], sha_1_b[30], sha_1_b[34], sha_1_b[30], sha_1_b[23], sha_1_b[12], sha_1_b[20],
+                    sha_1_b[20], sha_1_b[26], sha_1_b[17], sha_1_b[35],
+                    sha_1_b[5], sha_1_b[16], sha_1_b[37], sha_1_b[1]]) - 956
     headers = {}
-    headers["sign"] = "6:{}:{:x}:609184ae".format(
+    headers["sign"] = "7:{}:{:x}:6092b93d".format(
         sha_1_sign, abs(checksum))
     headers["time"] = time2
     return headers
@@ -139,15 +139,14 @@ class auth_details():
     def __init__(self, option: dict = {}):
         self.username = option.get('username', "")
         self.auth_id = option.get('auth_id', "")
+        self.sess = option.get('sess', "")
+        self.user_agent = option.get('user_agent', "")
         self.auth_hash = option.get('auth_hash', "")
         self.auth_uniq_ = option.get('auth_uniq_', "")
-        self.sess = option.get('sess', "")
         self.x_bc = option.get('x_bc', "")
-        self.app_token = option.get(
-            'app_token', '33d57ade8c02dbc5a333db99ff9ae26a')
-        self.user_agent = option.get('user_agent', "")
         self.email = option.get('email', "")
         self.password = option.get('password', "")
+        self.hashed = option.get('hashed', False)
         self.support_2fa = option.get('support_2fa', True)
         self.active = option.get('active', True)
 

00b6600b2d062a12e9c48a42b67722f4e21e4fc8
CRIMINAL <89371864+DIGITALCRIMINALS@users.noreply.github.com>
2021-08-25 05:52:53 +0000

    Update updater.py

--- updater.py
+++ updater.py
@@ -11,9 +11,9 @@ import time
 # response = requests.get("https://api.github.com/repos/digitalcriminal/onlyfans/branches/master")
 # response_json = response.json()
 # commit_id = response_json["commit"]["sha"]
-# downloaded = requests.get(f"https://github.com/DIGITALCRIMINALS/OnlyFans/archive/{commit_id}.zip")
+# downloaded = requests.get(f"https://github.com/DIGITALCRIMINAL/OnlyFans/archive/{commit_id}.zip")
 downloaded = requests.get(
-    f"https://github.com/DIGITALCRIMINAL/OnlyFans/archive/refs/heads/master.zip"
+    f"https://github.com/DIGITALCRIMINALS/OnlyFans/archive/refs/heads/master.zip"
 )
 content = io.BytesIO(downloaded.content)
 # Zip download for manual extraction

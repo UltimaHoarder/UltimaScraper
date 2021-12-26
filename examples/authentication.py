57434ac1cebae549db4c522481953a0a743d35f2
DIGITALCRIMINAL <89371864+DIGITALCRIMINALS@users.noreply.github.com>
2021-12-26 20:53:04 +0000

    [API] Refined example of authentication process

--- examples/authentication.py
+++ examples/authentication.py
@@ -4,13 +4,16 @@ import asyncio
 
 
 async def example():
-    onlyfans_api = OnlyFans_API()
-    auth = onlyfans_api.add_auth()
-    authed = await auth.login(guest=True)
+    async def authenticate():
+        onlyfans_api = OnlyFans_API()
+        auth = onlyfans_api.add_auth()
+        authed = await auth.login(guest=True)
+        return authed
+    authed = await authenticate()
     user = await authed.get_user("cummies")
     if isinstance(user, create_user):
         # Do stuff with user
         pass
 
-
-asyncio.run(example())
+if __name__ == "__main__":
+    asyncio.run(example())

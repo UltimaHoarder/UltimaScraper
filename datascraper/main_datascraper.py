f9f356a1b6fed8a66a1954ae3f6083202b6fe0c6
DIGITALCRIMINAL <89371864+DIGITALCRIMINALS@users.noreply.github.com>
2022-01-09 15:16:39 +0000

    Redid main_datascraper.py

--- datascraper/main_datascraper.py
+++ datascraper/main_datascraper.py
@@ -1,8 +1,9 @@
-import os
 import timeit
 from typing import Any, Optional
-from classes.make_settings import Config
 
+import apis.fansly.classes as fansly_classes
+import apis.onlyfans.classes as onlyfans_classes
+import apis.starsavn.classes as starsavn_classes
 import helpers.main_helper as main_helper
 import modules.fansly as m_fansly
 import modules.onlyfans as m_onlyfans
@@ -10,10 +11,14 @@ import modules.starsavn as m_starsavn
 from apis.fansly import fansly as Fansly
 from apis.onlyfans import onlyfans as OnlyFans
 from apis.starsavn import starsavn as StarsAVN
-from helpers.main_helper import OptionsFormat, choose_option
-import apis.fansly.classes as fansly_classes
-import apis.onlyfans.classes as onlyfans_classes
-import apis.starsavn.classes as starsavn_classes
+from classes.make_settings import Config
+from helpers.main_helper import OptionsFormat, account_setup
+
+auth_types = (
+    onlyfans_classes.auth_model.create_auth
+    | fansly_classes.auth_model.create_auth
+    | starsavn_classes.auth_model.create_auth
+)
 user_types = (
         onlyfans_classes.user_model.create_user
         | fansly_classes.user_model.create_user
@@ -25,216 +30,89 @@ api_helper = OnlyFans.api_helper
 async def start_datascraper(
     json_config: dict[Any, Any],
     site_name_lower: str,
-    api: Optional[OnlyFans.start | Fansly.start | StarsAVN.start] = None,
+    api_: Optional[OnlyFans.start | Fansly.start | StarsAVN.start] = None,
     webhooks: bool = True,
-) -> Optional[OnlyFans.start]:
+):
     json_settings = json_config["settings"]
-    json_sites = json_config["supported"]
     main_helper.assign_vars(json_config)
 
-    json_site_settings = json_sites[site_name_lower]["settings"]
-
-    auto_model_choice = json_site_settings["auto_model_choice"]
-    if isinstance(auto_model_choice, str):
-        temp_identifiers = auto_model_choice.split(",")
-        identifiers = [x for x in temp_identifiers if x]
-    else:
-        identifiers = []
-    auto_profile_choice = json_site_settings["auto_profile_choice"]
     proxies: list[str] = await api_helper.test_proxies(json_settings["proxies"])
     if json_settings["proxies"] and not proxies:
         print("Unable to create session")
         return None
+    async def default(datascraper: m_onlyfans.OnlyFansDataScraper|m_fansly.FanslyDataScraper|m_starsavn.StarsAVNDataScraper):
+        api = datascraper.api
+        site_settings = api.get_site_settings()
+        if not site_settings:
+            return
+        subscription_array:list[user_types] = []
+        auth_count = 0
+        profile_options = OptionsFormat(api.auths, "profiles",site_settings.auto_profile_choice)
+        api.auths = profile_options.final_choices
+        for auth in api.auths:
+            auth:auth_types = auth
+            if not auth.auth_details:
+                continue
+            setup = False
+            setup, subscriptions = await account_setup(
+                auth, datascraper, site_settings,
+            )
+            if not setup:
+                if webhooks:
+                    await main_helper.process_webhooks(
+                        api, "auth_webhook", "failed"
+                    )
+                auth_details:dict[str,Any] = {}
+                auth_details["auth"] = auth.auth_details.export()
+                profile_directory = auth.directory_manager.profile.root_directory
+                user_auth_filepath = profile_directory.joinpath("auth.json")
+                main_helper.export_data(auth_details, user_auth_filepath)
+                continue
+            auth_count += 1
+            subscription_array.extend(subscriptions)
+            await main_helper.process_webhooks(api, "auth_webhook", "succeeded")
+            # Do stuff with authed user
+        subscription_options = OptionsFormat(subscription_array, "subscriptions",site_settings.auto_model_choice)
+        subscription_list = subscription_options.final_choices
+        if site_settings.jobs.scrape.paid_content and api.has_active_auths():
+            print("Scraping Paid Content")
+            for authed in api.auths:
+                await datascraper.paid_content_scraper(authed)
+        if site_settings.jobs.scrape.subscriptions and api.has_active_auths():
+            print("Scraping Subscriptions")
+            await main_helper.process_jobs(
+                datascraper,
+                subscription_list,
+            )
+        await main_helper.process_downloads(api, datascraper)
+        if webhooks:
+            await main_helper.process_webhooks(api, "download_webhook", "succeeded")
     archive_time = timeit.default_timer()
     match site_name_lower:
         case "onlyfans":
-            site_name = "OnlyFans"
-            module = m_onlyfans
-            datascraper = m_onlyfans.OnlyFansDataScraper()
-            if not isinstance(api, OnlyFans.start):
-                api = OnlyFans.start(max_threads=json_settings["max_threads"],config=Config(**json_config))
+            if not isinstance(api_, OnlyFans.start):
+                api_ = OnlyFans.start(max_threads=json_settings["max_threads"],config=Config(**json_config))
                 main_helper.process_profiles(
-                    json_settings, proxies, site_name, api
+                    json_settings, proxies, api_
                 )
-            site_settings = api.get_site_settings()
-            if not site_settings:
-                return
-            subscription_array:list[user_types] = []
-            auth_count = 0
-            profile_options = OptionsFormat(api.auths, "profiles",auto_profile_choice)
-            api.auths = profile_options.final_choices
-            for auth in api.auths:
-                if not auth.auth_details:
-                    continue
-                setup = False
-                setup, subscriptions = await module.account_setup(
-                    auth, site_settings,identifiers, auth_count
-                )
-                if not setup:
-                    if webhooks:
-                        await main_helper.process_webhooks(
-                            api, "auth_webhook", "failed"
-                        )
-                    auth_details:dict[str,Any] = {}
-                    auth_details["auth"] = auth.auth_details.export()
-                    profile_directory = auth.directory_manager.profile.root_directory
-                    user_auth_filepath = profile_directory.joinpath("auth.json")
-                    main_helper.export_data(auth_details, user_auth_filepath)
-                    continue
-                auth_count += 1
-                subscription_array.extend(subscriptions)
-                await main_helper.process_webhooks(api, "auth_webhook", "succeeded")
-                # Do stuff with authed user
-            subscription_options = OptionsFormat(subscription_array, "subscriptions",auto_model_choice)
-            subscription_list = subscription_options.final_choices
-            if site_settings.jobs.scrape.paid_content and api.has_active_auths():
-                print("Scraping Paid Content")
-                await module.paid_content_scraper(api, identifiers)
-            if site_settings.jobs.scrape.subscriptions and api.has_active_auths():
-                print("Scraping Subscriptions")
-                await main_helper.process_names(
-                    datascraper,
-                    subscription_list,
-                    api,
-                )
-            await main_helper.process_downloads(api, datascraper)
-            if webhooks:
-                await main_helper.process_webhooks(api, "download_webhook", "succeeded")
+            datascraper = m_onlyfans.OnlyFansDataScraper(api_)
+            await default(datascraper)
         case "fansly":
-            site_name = "Fansly"
-            module = m_fansly
-            if not api:
-                api = Fansly.start(max_threads=json_settings["max_threads"])
-                api.settings = json_config
-                api = main_helper.process_profiles(
-                    json_settings, proxies, site_name, api
+            if not isinstance(api_, Fansly.start):
+                api_ = Fansly.start(max_threads=json_settings["max_threads"],config=Config(**json_config))
+                main_helper.process_profiles(
+                    json_settings, proxies, api_
                 )
-
-            subscription_array = []
-            auth_count = 0
-            jobs = json_site_settings["jobs"]
-            subscription_list = module.format_options(api.auths, "users")
-            if not auto_profile_choice:
-                print("Choose Profile")
-            auths = choose_option(subscription_list, auto_profile_choice, True)
-            api.auths = [x.pop(0) for x in auths]
-            for auth in api.auths:
-                if not auth.auth_details:
-                    continue
-                module.assign_vars(
-                    auth.auth_details, json_config, json_site_settings, site_name
-                )
-                setup = False
-                setup, subscriptions = await module.account_setup(
-                    auth, identifiers, jobs, auth_count
-                )
-                if not setup:
-                    if webhooks:
-                        await main_helper.process_webhooks(
-                            api, "auth_webhook", "failed"
-                        )
-                    auth_details = {}
-                    auth_details["auth"] = auth.auth_details.export()
-                    profile_directory = auth.profile_directory
-                    if profile_directory:
-                        user_auth_filepath = os.path.join(
-                            auth.profile_directory, "auth.json"
-                        )
-                        main_helper.export_data(auth_details, user_auth_filepath)
-                    continue
-                auth_count += 1
-                subscription_array += subscriptions
-                await main_helper.process_webhooks(api, "auth_webhook", "succeeded")
-                # Do stuff with authed user
-            subscription_list = module.format_options(
-                subscription_array, "usernames", api.auths
-            )
-            site_settings = api.get_site_settings()
-            if site_settings.jobs.scrape.paid_content and api.has_active_auths():
-                print("Scraping Paid Content")
-                # await module.paid_content_scraper(api, identifiers)
-            site_settings = api.get_site_settings()
-            if site_settings.jobs.scrape.subscriptions and api.has_active_auths():
-                print("Scraping Subscriptions")
-                await main_helper.process_names(
-                    module,
-                    subscription_list,
-                    auto_model_choice,
-                    api,
-                    json_config,
-                    site_name_lower,
-                    site_name,
-                )
-            await main_helper.process_downloads(api, module)
-            if webhooks:
-                await main_helper.process_webhooks(api, "download_webhook", "succeeded")
+            datascraper = m_fansly.FanslyDataScraper(api_)
+            await default(datascraper)
         case "starsavn":
-            site_name = "StarsAVN"
-            module = m_starsavn
-            if not api:
-                api = StarsAVN.start(max_threads=json_settings["max_threads"])
-                api.settings = json_config
-                api = main_helper.process_profiles(
-                    json_settings, proxies, site_name, api
+            if not isinstance(api_, StarsAVN.start):
+                api_ = StarsAVN.start(max_threads=json_settings["max_threads"],config=Config(**json_config))
+                main_helper.process_profiles(
+                    json_settings, proxies, api_
                 )
-                print
-
-            subscription_array = []
-            auth_count = 0
-            jobs = json_site_settings["jobs"]
-            subscription_list = module.format_options(api.auths, "users")
-            if not auto_profile_choice:
-                print("Choose Profile")
-            auths = choose_option(subscription_list, auto_profile_choice, True)
-            api.auths = [x.pop(0) for x in auths]
-            for auth in api.auths:
-                if not auth.auth_details:
-                    continue
-                module.assign_vars(
-                    auth.auth_details, json_config, json_site_settings, site_name
-                )
-                setup = False
-                setup, subscriptions = await module.account_setup(
-                    auth, identifiers, jobs, auth_count
-                )
-                if not setup:
-                    if webhooks:
-                        await main_helper.process_webhooks(
-                            api, "auth_webhook", "failed"
-                        )
-                    auth_details = {}
-                    auth_details["auth"] = auth.auth_details.export()
-                    profile_directory = auth.profile_directory
-                    if profile_directory:
-                        user_auth_filepath = os.path.join(
-                            auth.profile_directory, "auth.json"
-                        )
-                        main_helper.export_data(auth_details, user_auth_filepath)
-                    continue
-                auth_count += 1
-                subscription_array += subscriptions
-                await main_helper.process_webhooks(api, "auth_webhook", "succeeded")
-                # Do stuff with authed user
-            subscription_list = module.format_options(
-                subscription_array, "usernames", api.auths
-            )
-            if jobs["scrape_paid_content"] and api.has_active_auths():
-                print("Scraping Paid Content")
-                # await module.paid_content_scraper(api, identifiers)
-            if jobs["scrape_names"] and api.has_active_auths():
-                print("Scraping Subscriptions")
-                await main_helper.process_names(
-                    module,
-                    subscription_list,
-                    auto_model_choice,
-                    api,
-                    json_config,
-                    site_name_lower,
-                    site_name,
-                )
-            await main_helper.process_downloads(api, module)
-            if webhooks:
-                await main_helper.process_webhooks(api, "download_webhook", "succeeded")
+            datascraper = m_starsavn.StarsAVNDataScraper(api_)
+            await default(datascraper)
     stop_time = str(int(timeit.default_timer() - archive_time) / 60)[:4]
     print("Archive Completed in " + stop_time + " Minutes")
-    return api
+    return api_
